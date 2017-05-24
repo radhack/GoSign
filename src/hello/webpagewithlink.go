321394a269812a9863c30cf39380f8ffbc8e1cf3
Alex Griffen <radhack242@gmail.com>
2017-05-24 00:51:46 -0700

    update webpagewithlink.go to use an environment variable for api key (and changed my api key you sneaky people)

--- src/hello/webpagewithlink.go
+++ src/hello/webpagewithlink.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 	"net/http"
 	"io/ioutil"
+	// "syscall"
+	"os"
 	// "encoding/json"
 )
 
@@ -15,6 +17,8 @@ func main() {
 
 	http.ListenAndServe(":6789", nil)
 
+
+
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -24,8 +28,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func handler2(w http.ResponseWriter, r *http.Request) {
+	API_GOLONG_KEY := os.Getenv("API_GOLONG_KEY")
 
-	url := "https://:@api.hellosign.com/v3/signature_request/send_with_template"
+	url := "https://" + API_GOLONG_KEY + "@api.hellosign.com/v3/signature_request/send_with_template"
 
 	payload := strings.NewReader("------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"client_id\"\r\n\r\nd7219512693825facdd9241f458decf2\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"template_id\"\r\n\r\n06874015c4197f5b8d7a03f159095385d89c74d5\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"test_mode\"\r\n\r\n1\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"subject\"\r\n\r\nusing postman\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"message\"\r\n\r\ntemplate non embedded\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"signers[Signer][email_address]\"\r\n\r\nalex+postman1@hellosign.com\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"signers[Signer][name]\"\r\n\r\nMr Postman\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"custom_fields\"\r\n\r\n[{ \"name\":\"label1\", \"value\":\"things here aren't always what they seem\" } ]\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--")
 
